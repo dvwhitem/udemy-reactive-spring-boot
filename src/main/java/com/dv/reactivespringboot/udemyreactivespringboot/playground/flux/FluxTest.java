65f31f9afe9793e6109389458223040caaecbafb
dvwhitem <dvwhitem@gmail.com>
2021-09-01 14:40:21 +0300

    add Flux test

--- src/main/java/com/dv/reactivespringboot/udemyreactivespringboot/playground/flux/FluxTest.java
+++ src/main/java/com/dv/reactivespringboot/udemyreactivespringboot/playground/flux/FluxTest.java
@@ -1,11 +1,17 @@
 package com.dv.reactivespringboot.udemyreactivespringboot.playground.flux;
 
 import org.junit.Test;
+import reactor.core.publisher.Flux;
 
 public class FluxTest {
 
     @Test
     public void fluxTest() {
 
+      Flux<String> reactiveFlux = Flux.just("Spring Boot", "Reactive Spring", "Reactive Kafka")
+              //.concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+              .concatWith(Flux.just("After Error"))
+              .log();
+      reactiveFlux.subscribe(System.out::println, e -> System.err.println("Exception is "+ e), () -> System.out.println("Completed"));
     }
 }
