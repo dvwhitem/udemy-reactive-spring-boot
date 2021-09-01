5502ce9a07f27ba114afed689d593a76c041634b
Dynisyuk Vitaliy <dvwhitem@gmail.com>
2021-09-01 13:24:51 +0300

    first commit

---
+++ src/main/java/com/dv/reactivespringboot/udemyreactivespringboot/UdemyReactiveSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dv.reactivespringboot.udemyreactivespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyReactiveSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyReactiveSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dv/reactivespringboot/udemyreactivespringboot/UdemyReactiveSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dv.reactivespringboot.udemyreactivespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyReactiveSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
