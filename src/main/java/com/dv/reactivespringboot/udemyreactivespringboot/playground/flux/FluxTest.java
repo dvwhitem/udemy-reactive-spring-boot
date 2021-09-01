084e9bdc77f2ba2b6cc03d45d7b09693a375c4f9
dvwhitem <dvwhitem@gmail.com>
2021-09-01 14:06:45 +0300

    polish

--- src/main/java/com/dv/reactivespringboot/udemyreactivespringboot/playground/flux/FluxTest.java
+++ src/main/java/com/dv/reactivespringboot/udemyreactivespringboot/playground/flux/FluxTest.java
@@ -1,4 +1,11 @@
 package com.dv.reactivespringboot.udemyreactivespringboot.playground.flux;
 
+import org.junit.Test;
+
 public class FluxTest {
+
+    @Test
+    public void fluxTest() {
+
+    }
 }
