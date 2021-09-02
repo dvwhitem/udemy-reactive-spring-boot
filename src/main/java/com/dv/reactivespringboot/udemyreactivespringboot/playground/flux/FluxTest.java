8984d90b194d02dc7e53af11eb50a4a38d61aed0
dvwhitem <dvwhitem@gmail.com>
2021-09-02 16:22:52 +0300

    add unit test Flux, Momo

--- src/main/java/com/dv/reactivespringboot/udemyreactivespringboot/playground/flux/FluxTest.java
+++ src/main/java/com/dv/reactivespringboot/udemyreactivespringboot/playground/flux/FluxTest.java
@@ -2,6 +2,7 @@ package com.dv.reactivespringboot.udemyreactivespringboot.playground.flux;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxTest {
 
@@ -13,5 +14,56 @@ public class FluxTest {
               .concatWith(Flux.just("After Error"))
               .log();
       reactiveFlux.subscribe(System.out::println, e -> System.err.println("Exception is "+ e), () -> System.out.println("Completed"));
+    }
+
+    @Test
+    public void fluxTestElementsWithoutError() {
+        Flux<String> stringFlux = Flux.just("Spring Boot", "Spring Reactive", "Reactive Kafka")
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring Boot")
+                .expectNext("Spring Reactive")
+                .expectNext("Reactive Kafka")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxTestElementsWithError() {
+        Flux<String> stringFlux = Flux.just("Spring Boot", "Spring Reactive", "Reactive Kafka")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring Boot")
+                .expectNext("Spring Reactive")
+                .expectNext("Reactive Kafka")
+                //.expectError(RuntimeException.class)
+                .expectErrorMessage("Exception Occurred")
+                .verify();
+    }
+
+    @Test
+    public void fluxTestElementsWithError2() {
+        Flux<String> stringFlux = Flux.just("Spring Boot", "Spring Reactive", "Reactive Kafka")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring Boot", "Spring Reactive", "Reactive Kafka")
+                .expectErrorMessage("Exception Occurred")
+                .verify();
+    }
+
+    @Test
+    public void fluxTestElementsCountWithError() {
+        Flux<String> stringFlux = Flux.just("Spring Boot", "Spring Reactive", "Reactive Kafka")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(3)
+                .expectErrorMessage("Exception Occurred")
+                .verify();
     }
 }
--- src/main/java/com/dv/reactivespringboot/udemyreactivespringboot/playground/mono/MonoTest.java
+++ src/main/java/com/dv/reactivespringboot/udemyreactivespringboot/playground/mono/MonoTest.java
@@ -1,4 +1,23 @@
 package com.dv.reactivespringboot.udemyreactivespringboot.playground.mono;
 
+import org.junit.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
 public class MonoTest {
+
+    @Test
+    public void monoTest() {
+       Mono<String> stringMono = Mono.just("Spring").log();
+        StepVerifier.create(stringMono)
+                .expectNext("Spring")
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoTestError() {
+        StepVerifier.create(Mono.error(new RuntimeException("Exception Occurred")).log())
+                .expectError(RuntimeException.class)
+                .verify();
+    }
 }
