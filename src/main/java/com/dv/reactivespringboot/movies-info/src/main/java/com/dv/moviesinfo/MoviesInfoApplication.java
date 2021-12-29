1ee0570aaa881f7f60a32ce80b9ad94a4026d447
dvwhitem <dvwhitem@gmail.com>
2021-12-29 17:04:51 +0200

    add movies info service

---
+++ src/main/java/com/dv/reactivespringboot/movies-info/src/main/java/com/dv/moviesinfo/MoviesInfoApplication.java
@@ -0,0 +1,13 @@
+package com.dv.moviesinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesInfoApplication.class, args);
+    }
+
+}
